ec5c22c330883d728c381db5774308f577531f54
Saad Ibraheem <saadibraheem621@gmail.com>
2026-05-08 14:37:10 +0300

    fix https redirect

--- app.py
+++ app.py
@@ -188,7 +188,11 @@ def logout():
 
 @app.route("/google-login")
 def google_login():
-    redirect_uri = url_for("google_callback", _external=True)
+    redirect_uri = url_for(
+    "google_callback",
+    _external=True,
+    _scheme="https"
+)
     return google.authorize_redirect(redirect_uri)
 
 
