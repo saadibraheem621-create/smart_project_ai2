b03797273f200e952de943512eb75e5ab3482883
Saad Ibraheem <saadibraheem621@gmail.com>
2026-05-14 00:19:55 +0300

    fix upload limit

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import uuid
 
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "zenvy-secret")
 app.config["UPLOAD_FOLDER"] = "static/uploads"
