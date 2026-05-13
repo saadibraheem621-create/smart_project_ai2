9f50b7152b72b5c086cd3998eb300398b7599956
Saad Ibraheem <saadibraheem621@gmail.com>
2026-05-13 23:01:59 +0300

    fix indentation

--- app.py
+++ app.py
@@ -207,10 +207,10 @@ def data_analysis():
         filename = secure_filename(file.filename)
         path = os.path.join("static/data_files", filename)
         file.save(path)
-     try:
-    df = pd.read_csv(path, encoding="utf-8")
-except:
-    df = pd.read_csv(path, encoding="latin1")
+        try:
+            df = pd.read_csv(path, encoding="utf-8")
+        except:
+            df = pd.read_csv(path, encoding="latin1")
 
     result = {
         "rows": df.shape[0],
