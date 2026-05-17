e30b8478bc9fea9861ec485b6b7e8dfffb731d76
Saad Ibraheem <saadibraheem621@gmail.com>
2026-05-17 17:53:12 +0300

    add fix db route

--- app.py
+++ app.py
@@ -176,6 +176,11 @@ def generate_ai_product_image(title, description):
 
     return filename
 
+@app.route("/fix-db")
+def fix_db():
+    db.create_all()
+    return "Database updated"
+
 
 @app.context_processor
 def inject_user():
